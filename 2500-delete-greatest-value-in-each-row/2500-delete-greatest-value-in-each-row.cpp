68541f7c985b560653c259b5be30d16f72ed2e8a
Vivek Rai <75068627+Vivekrai-56@users.noreply.github.com>
2023-04-05 15:24:26 +0530

    Time: 15 ms (64.84%), Space: 9.5 MB (27.14%) - LeetHub

--- 2500-delete-greatest-value-in-each-row/2500-delete-greatest-value-in-each-row.cpp
+++ 2500-delete-greatest-value-in-each-row/2500-delete-greatest-value-in-each-row.cpp
@@ -6,7 +6,7 @@ public:
         
         int ans=0;
         for(int i=0;i<rows;i++){
-            sort(grid[i].begin(),grid[i].end(),greater<int>());
+            sort(grid[i].begin(),grid[i].end());
         }
         
         for(int i=0;i<cols;i++){
